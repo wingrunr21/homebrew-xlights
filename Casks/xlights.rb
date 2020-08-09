ab789e9fbb20ede2555872d25a7b4b47e0bdc35e
wingrunr21 <wingrunr21@github.com>
2020-08-09 22:05:21 +0000

    Update xLights to version 2020.31

--- Casks/xlights.rb
+++ Casks/xlights.rb
@@ -1,6 +1,6 @@
 cask 'xlights' do
-  version '2020.29'
-  sha256 'd144e4db0990af034610c70c5861ea729aae762b817761969fd0d49b563d2fe6'
+  version '2020.31'
+  sha256 '1440bd11f93c6960d1bb5f95176f8da506d777c134d784eccab82062d4404a72'
 
   name 'xLights'
   homepage 'https://xlights.org/'
